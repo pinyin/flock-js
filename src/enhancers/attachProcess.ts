e55768a8c3009a7be4e8ced206cf895d8055367c
pinyin <bob432091@gmail.com>
2019-01-14 18:28:59 +0800

    clean up code

--- src/enhancers/attachProcess.ts
+++ src/enhancers/attachProcess.ts
@@ -1,11 +1,4 @@
-import {
-    Store,
-    StoreCreator,
-    StoreEnhancer,
-    StoreForEnhancer,
-    Subscriber,
-    Unsubscribe,
-} from '../types'
+import { Store, StoreCreator, StoreEnhancer, StoreForEnhancer, Unsubscribe } from '../types'
 
 export function attachProcess<E>(...processes: Process<E>[]): StoreEnhancer<E> {
     return (inner: StoreCreator<E>) => (
@@ -13,24 +6,15 @@ export function attachProcess<E>(...processes: Process<E>[]): StoreEnhancer<E> {
     ): StoreForEnhancer<E> => {
         const _terminators = new Set()
 
-        const _listeners = new Set<Subscriber>()
         const innerStore = inner(prepublish)
 
         const store: StoreForEnhancer<E> = {
             ...innerStore,
-            subscribe(subscriber: Subscriber) {
-                _listeners.add(subscriber)
-                return () => _listeners.delete(subscriber)
-            },
             replaceEvents: (events: Array<E>, cursor: number): void => {
                 _terminators.forEach(terminate => terminate()) // TODO try-catch
                 _terminators.clear()
                 innerStore.replaceEvents(events, cursor)
                 processes.forEach(process => _terminators.add(process(store)))
-            },
-            dispatch: (event: E) => {
-                innerStore.dispatch(event)
-                _listeners.forEach(it => it()) // TODO try-catch
             },
         }
 
