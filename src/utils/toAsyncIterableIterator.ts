cf308d9247c4df530612368a88f9ca0d3452847b
pinyin <bob432091@gmail.com>
2019-01-27 11:13:18 +0800

    refactor toAsyncIterableIterator

--- src/utils/toAsyncIterableIterator.ts
+++ src/utils/toAsyncIterableIterator.ts
@@ -1,26 +1,24 @@
 import { Observable } from 'rxjs'
 import { first } from 'rxjs/internal/operators/first'
-import { tap } from 'rxjs/internal/operators/tap'
 import { async } from 'rxjs/internal/scheduler/async'
 import { observeOn, share } from 'rxjs/operators'
 
 export async function* toAsyncIterableIterator<T>(
     source: Observable<T>,
 ): AsyncIterableIterator<T> {
-    let cache = new Array<T>()
     const sharedSource = source.pipe(
-        tap(v => cache.push(v)),
         observeOn(async),
         share(),
     )
     const sharingSource = sharedSource.subscribe()
-    const hasNext = async () =>
-        (await sharedSource.pipe(first(null, NONE)).toPromise()) !== NONE
-    do {
-        yield* cache
-        cache = []
-    } while (await hasNext())
-    sharingSource.unsubscribe()
+    const getNext = () => sharedSource.pipe(first(null, NONE)).toPromise()
+    try {
+        for (let v = await getNext(); v !== NONE; v = await getNext()) {
+            yield v
+        }
+    } finally {
+        sharingSource.unsubscribe()
+    }
 }
 
 const NONE = Symbol('NONE')
