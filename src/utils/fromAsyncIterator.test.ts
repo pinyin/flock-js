1d90207d1589a7377a6e0bf1660b70bf85dd30f0
pinyin <bob432091@gmail.com>
2019-01-30 09:54:27 +0800

    add test to fromAsyncIterator

--- src/utils/fromAsyncIterator.test.ts
+++ src/utils/fromAsyncIterator.test.ts
@@ -16,4 +16,20 @@ describe(`${fromAsyncIterator.name}`, ()=> {
         expect(await obs.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
         expect(await obs.pipe(take(1), toArray()).toPromise()).toEqual([4])
     })
+
+    it(`should share source iterator across all subscriptions`, async ()=> {
+        const source = [1, 2, 3, 4]
+        async function* source_() {
+            yield source[0]
+            await Promise.resolve()
+            yield source[1]
+            yield source[2]
+            await Promise.resolve()
+            yield source[3]
+        }
+        const obs1 = fromAsyncIterator(source_())
+        const obs2 = fromAsyncIterator(source_())
+        expect(await obs1.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
+        expect(await obs2.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
+    })
 })
