9c3a762eeb0be7fcf76b431c23e20a3a11646450
pinyin <bob432091@gmail.com>
2019-01-30 12:15:34 +0800

    share iterator across fromAsyncIterator subscriptions

--- src/utils/fromAsyncIterator.test.ts
+++ src/utils/fromAsyncIterator.test.ts
@@ -1,9 +1,10 @@
 import { take, toArray } from 'rxjs/operators'
 import { fromAsyncIterator } from './fromAsyncIterator'
 
-describe(`${fromAsyncIterator.name}`, ()=> {
-    it(`should convert async iterator to observable`, async ()=> {
+describe(`${fromAsyncIterator.name}`, () => {
+    it(`should convert async iterator to observable`, async () => {
         const source = [1, 2, 3, 4]
+
         async function* source_() {
             yield source[0]
             await Promise.resolve()
@@ -12,13 +13,47 @@ describe(`${fromAsyncIterator.name}`, ()=> {
             await Promise.resolve()
             yield source[3]
         }
+
         const obs = fromAsyncIterator(source_())
-        expect(await obs.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
-        expect(await obs.pipe(take(1), toArray()).toPromise()).toEqual([4])
+        expect(
+            await obs
+                .pipe(
+                    take(3),
+                    toArray(),
+                )
+                .toPromise(),
+        ).toEqual([1, 2, 3])
+        expect(
+            await obs
+                .pipe(
+                    take(1),
+                    toArray(),
+                )
+                .toPromise(),
+        ).toEqual([])
+
+        const obs2 = fromAsyncIterator(source_(), false)
+        expect(
+            await obs2
+                .pipe(
+                    take(3),
+                    toArray(),
+                )
+                .toPromise(),
+        ).toEqual([1, 2, 3])
+        expect(
+            await obs2
+                .pipe(
+                    take(1),
+                    toArray(),
+                )
+                .toPromise(),
+        ).toEqual([4])
     })
 
-    it(`should share source iterator across all subscriptions`, async ()=> {
+    it(`should share iterator across concurrent subscriptions`, async () => {
         const source = [1, 2, 3, 4]
+
         async function* source_() {
             yield source[0]
             await Promise.resolve()
@@ -27,9 +62,22 @@ describe(`${fromAsyncIterator.name}`, ()=> {
             await Promise.resolve()
             yield source[3]
         }
-        const obs1 = fromAsyncIterator(source_())
-        const obs2 = fromAsyncIterator(source_())
-        expect(await obs1.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
-        expect(await obs2.pipe(take(3), toArray()).toPromise()).toEqual([1, 2, 3])
+
+        const iterator = source_()
+        const obs = fromAsyncIterator(iterator)
+        const r1 = obs
+            .pipe(
+                take(3),
+                toArray(),
+            )
+            .toPromise()
+        const r2 = obs
+            .pipe(
+                take(3),
+                toArray(),
+            )
+            .toPromise()
+        expect(await r1).toEqual([1, 2, 3])
+        expect(await r2).toEqual([1, 2, 3])
     })
 })
--- src/utils/fromAsyncIterator.ts
+++ src/utils/fromAsyncIterator.ts
@@ -1,30 +1,52 @@
 import { Observable, Subscriber } from 'rxjs'
 
-export function fromAsyncIterator<T>(source: AsyncIterator<T>): Observable<T> {
-    let next = source.next()
+export function fromAsyncIterator<T>(
+    source: AsyncIterator<T>,
+    returnOnUnsubscribe: boolean = true,
+): Observable<T> {
+    const subscribers = new Set<Subscriber<T>>()
+    function startForwardingTo(ref: Subscriber<T>) {
+        subscribers.add(ref)
+        if (subscribers.size === 1) startForwarding()
+    }
+    function stopForwardingTo(ref: Subscriber<T>) {
+        subscribers.delete(ref)
+        if (
+            returnOnUnsubscribe &&
+            subscribers.size === 0 &&
+            typeof source.return === 'function'
+        )
+            source.return()
+    }
+
+    async function startForwarding() {
+        try {
+            for (
+                let next = await source.next();
+                !next.done;
+                next = await source.next()
+            ) {
+                for (const s of subscribers) {
+                    s.next(next.value)
+                }
+            }
+            for (const s of subscribers) {
+                s.complete()
+            }
+        } catch (e) {
+            for (const s of subscribers) {
+                s.error(e)
+            }
+        } finally {
+            if (typeof source.return === 'function') source.return()
+        }
+    }
 
     return Observable.create((subscriber: Subscriber<T>) => {
-        let unsubscribed: boolean = false
-
-        async function receive() {
-            try {
-                for (
-                    ;
-                    !unsubscribed && !(await next).done;
-                    next = source.next()
-                ) {
-                    subscriber.next((await next).value)
-                }
-                subscriber.complete()
-            } catch (e) {
-                subscriber.error(e)
-            }
-        }
-
-        receive()
-
+        const ref = new Subscriber(subscriber)
+        startForwardingTo(ref)
         return () => {
-            unsubscribed = true
+            stopForwardingTo(ref)
         }
     })
 }
